2a601e603aa27667380e3067e8d5158b10f58168
21496 <22425@burnside.school.nz>
2024-05-06 11:36:18 +1200

    tested database

--- App.py
+++ App.py
@@ -1,1 +1,9 @@
-print("Goodbye, World")
+import sqlite3
+
+db = sqlite3.connect("fighters.db")
+cursor = db.cursor()
+sql = "SELECT * from fighter;"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close()
