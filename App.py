3c46c0e85ed2513167ee12bb3011bc1c974ba859
21496 <22425@burnside.school.nz>
2024-05-07 10:51:30 +1200

    Print all aircrafts

--- App.py
+++ App.py
@@ -1,9 +1,22 @@
+#docstring- Zac Newman- airplane database application
+#imports
 import sqlite3
 
-db = sqlite3.connect("fighters.db")
-cursor = db.cursor()
-sql = "SELECT * from fighter;"
-cursor.execute(sql)
-results = cursor.fetchall()
-print(results)
-db.close()
+#constants and variables
+DATABASE = "fighters.db"
+
+#functions
+def print_all_aircraft():
+    """print all the aircrafts nicely"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from fighter;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    for fighter in 
+    print(results)
+    db.close()
+
+#main code
+print_all_aircraft()
