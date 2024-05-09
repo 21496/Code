c5894cc607ab835e6228348ea846e93af9db8bd3
21496 <22425@burnside.school.nz>
2024-05-10 09:15:13 +1200

    Finished Code

--- App.py
+++ App.py
@@ -10,7 +10,7 @@ def print_all_aircraft():
     """print all the aircrafts nicely"""
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    sql = "SELECT * from fighter;"
+    sql = "SELECT * FROM fighter;"
     cursor.execute(sql)
     results = cursor.fetchall()
     #loop through all the results
@@ -20,13 +20,102 @@ def print_all_aircraft():
     #loop finished here
     db.close()
 
+def print_all_aircraft_by_speed():
+    """print all the aircrafts nicely"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter ORDER By speed DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name                          speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
+    
+def print_all_aircraft_by_max_g_force():
+    """print all the aircrafts nicely"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter ORDER by max_g DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name                          speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
+
+def print_all_aircraft_by_climb():
+    """print all the aircrafts nicely"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter ORDER by climbrate DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name                          speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
+
+def print_all_aircraft_by_range():
+    """print all the aircrafts nicely"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter ORDER by range DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name                          speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
+
+def print_all_aircraft_by_payload():
+    """print all the aircrafts nicely"""
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter ORDER by payload DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name                          speed   max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
 
 #main code
 while True:
-    user_input = input("\nWhat would you like to do.\n1. Print all aircraft\n2. Exit\n")
+    user_input = input(
+"""
+What would you like to do.
+1. Print all aircrafts
+2. Print all aircrafts sorted by speed
+3. Print all aircrafts sorted by max g force
+4. Print all aircrafts sorted by climb
+5. Print all aircrafts sorted by range
+6. Print all aircrafts sorted by payload
+7. Exit
+""")
     if user_input == "1":
         print_all_aircraft()
     elif user_input == "2":
+        print_all_aircraft_by_speed()
+    elif user_input == "3":
+        print_all_aircraft_by_max_g_force()
+    elif user_input == "4":
+        print_all_aircraft_by_climb()
+    elif user_input == "5":
+        print_all_aircraft_by_range()
+    elif user_input == "6":
+        print_all_aircraft_by_payload()
+    elif user_input == "7":
         break
     else:
         print("That was not an option\n")
